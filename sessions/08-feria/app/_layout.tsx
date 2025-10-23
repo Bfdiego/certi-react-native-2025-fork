2723ebdfc46e9c2f212631797aa4d8f91cebc752
Bfdiego <dayo.alba.p@gmail.com>
2025-10-23 11:39:13 -0400

    holaa

--- sessions/08-feria/app/_layout.tsx
+++ sessions/08-feria/app/_layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Stack } from 'expo-router';
+import { AuthProvider } from '../contexxt/authContext';
 // import * as WebBrowser from "expo-web-browser";
 // import { AuthProvider } from '../src/auth/authProvider';
 
@@ -9,11 +10,14 @@ import { Stack } from 'expo-router';
 
 const RootLayout = () => {
     return (
-            <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="login" />
-                <Stack.Screen name="signup" />
-                <Stack.Screen name="(tabs)" />
-            </Stack>
+            <AuthProvider>
+                <Stack screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name="login" />
+                    <Stack.Screen name="signup" />
+                    <Stack.Screen name="(tabs)" />
+                </Stack>
+            </AuthProvider>
+            
     )
 }
 export default RootLayout;
